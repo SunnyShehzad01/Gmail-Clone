ce3c9afc45af441aa3cf54493b3cb44c640e3c64
Shehzad khan <khan415072@gmail.com>
2024-02-02 23:35:15 +0530

    Added Email-Section

--- src/Components/EmailList.jsx
+++ src/Components/EmailList.jsx
@@ -1,8 +1,43 @@
+import { ArrowDropDown, ChevronLeft, ChevronRight, Inbox, Keyboard, LocalOffer, MoreVert, People, Redo, Settings } from '@mui/icons-material'
+import { Checkbox, IconButton } from '@mui/material'
 import React from 'react'
+import '../styles.css'
+import EmailSection from './EmailSection'
 
 const EmailList = () => {
+  const emailSectionDetails = [
+    {icon: Inbox, title: "Primary", color: "red", selected: true},
+    {icon: People, title: "Social", color: "#1a73e8", selected: false},
+    {icon: LocalOffer, title: "Promotions", color: "green"}
+  ]
   return (
-    <div>EmailList</div>
+    <div className='emailList'>
+      <div className="emailList-settings">
+        <div className="emailList-settings-left">
+          <Checkbox />
+          <IconButton > <ArrowDropDown /> </IconButton>
+          <IconButton> <Redo /> </IconButton>
+          <IconButton> <MoreVert /> </IconButton>
+        </div>
+
+        <div className="email-settings-right">
+          <IconButton> <ChevronLeft/> </IconButton>
+          <IconButton> <ChevronRight/> </IconButton>
+          <IconButton> <Keyboard /> </IconButton>
+          <IconButton> <Settings /> </IconButton>
+        </div>
+      </div>
+
+      <div className='emailList-sections'>
+        {
+          emailSectionDetails.map((detail, index) => {
+            return (
+              <EmailSection key={index} Icon={detail.icon} title={detail.title} color={detail.color} selected={detail.selected} />
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
